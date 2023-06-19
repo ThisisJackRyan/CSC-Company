1edd2c11c42da1dec736052f049e276dafbac881
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-06-18 20:16:07 -0500

    slider done

--- src/Components/Slider.jsx
+++ src/Components/Slider.jsx
@@ -31,11 +31,11 @@ const Slider = () => {
 
     const CaseSlide = (num) => {
         switch(num){
-            case 1: return(<Slide image={Mary} words ="Some Words"/>); 
-            case 2: return(<Slide image={Cerner} words="Some Words"/>); 
-            case 3: return(<Slide image={Rock} words ="Some Words"/>); 
-            case 4: return(<Slide image={Data} words ="Some Words"/>);
-            default: return(<Slide image={Mary} words ="Some Words"/>);
+            case 1: return(<Slide image={Mary} words ="Discover the value of automation systems in any environment"/>); 
+            case 2: return(<Slide image={Cerner} words="Reduce energy consumption without sacrificing comfort"/>); 
+            case 3: return(<Slide image={Rock} words ="Illuminate spaces with sleek design for optimal use"/>); 
+            case 4: return(<Slide image={Data} words ="Minimize security risks with cost-effective surveillance"/>);
+            default: return(<Slide image={Mary} words ="Discover the value of automation systems in any environment"/>);
         }
     }
     
