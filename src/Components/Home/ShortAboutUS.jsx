4d87732b3a5afd37876c98441bcdf3104d0a199d
ThisisJackRyan <thisisjackryan1@gmail.com>
2023-06-24 16:50:22 -0500

    polished css

--- src/Components/Home/ShortAboutUS.jsx
+++ src/Components/Home/ShortAboutUS.jsx
@@ -7,12 +7,15 @@ const ShortAboutUS = () => {
             <div className="blueBar">\/</div>
             <div className="miniAbout">
                 <div className="miniAboutWords">
-                    <div className="miniAboutTitle">Title</div>
+                    <div className="miniAboutTitle">Invest in your environment. Reap the benefits.</div>
                     <div className="miniAboutParagraph">
-                        <p>Words</p>
+                        <p>Controlling and operating facilities at peak efficiency is a complex job. From maintaining optimal comfort while maximizing energy savings to creating safe and well-lit spaces, there are a number of issues that must be addressed to keep your employees and customers happy.</p>
+                        <p>For more than 40 years, Control Service Company has put the latest technology and resources to work to help businesses operate more than 1,500 buildings effectively. We do so by pairing top-of-the-line building automation systems with exceptional service to successfully meet your facility management objectives. Now’s the time to get comfortable with a controls partner you can trust.</p>
                     </div>
                 </div>
-                <div className="flippingImages"></div>
+                <div className="flippingImages">
+                    
+                </div>
             </div>
         </div>
     )
